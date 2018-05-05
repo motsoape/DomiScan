a07dcb054ea4c7a4eb688a1e15646f56eee3b9fe
Collins <u12211070@tuks.co.za>
2018-05-05 16:47:57 +0200

    New Branch

--- app/src/main/java/com/dominantit/domiscan/Shared/MainActivity.java
+++ app/src/main/java/com/dominantit/domiscan/Shared/MainActivity.java
@@ -1,18 +1,27 @@
 package com.dominantit.domiscan.Shared;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.dominantit.domiscan.R;
+import com.dominantit.domiscan.Scan.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity  {
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+
+        viewPager.setAdapter(viewPagerAdapter);
     }
 
    /* @Override
